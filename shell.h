00eff2aa8fc6510d6169648bee408a4dd9d90478
Henshaw <josephhensh2000@gmail.com>
2023-09-20 13:16:21 -0700

    Update file

--- shell.h
+++ shell.h
@@ -19,15 +19,17 @@ char **getPath(char **env);
 char *get_full_cmd(char *path, char *command);
 void child_process(char **tokens, char *absolute_path,
 		char *shell, char **env, ssize_t *exit_status);
-int change_dir(const char *path);
+int change_dir(char *path);
 void exec_cmd(char **tokens, char *absolute_path, char *shell, char **env);
 void print_env(void);
+char *_getenv(char *name);
 void free_memory(char **buffer);
 int _strlen(char *s);
 char *_strcpy(char *dest, char *src);
 char *_strcat(char *dest, char *src);
 int _strcmp(char *s1, char *s2);
 char *_strdup(char *str);
+int _strncmp(char *s1, char *s2, int n);
 int _isdigit(char *num);
 int _atoi(char *s);
 void error_message(char **tokens, char *full_path, char *shell, size_t count);
